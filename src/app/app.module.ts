dfa1bcf9a1a9266ab8ee0b3b5446dd1a464c2150
JonathanLadwig <jonladwig98@gmail.com>
2023-03-26 08:57:40 +0200

    ci: :tada: Added Firebase with Login Components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,29 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getAuth, provideAuth } from '@angular/fire/auth';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
+import { environment } from '../environments/environment';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './components/login/login.component';
+import { RegisterComponent } from './components/register/register.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    RegisterComponent,
+    DashboardComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    provideFirebaseApp(() => initializeApp(environment.firebase)),
+    provideAuth(() => getAuth()),
+    provideFirestore(() => getFirestore())
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/environments/environment.development.ts
@@ -0,0 +1,9 @@
+export const environment = {
+  firebase: {
+    projectId: 'holiday-planner-8b6a7',
+    appId: '1:739095630197:web:66abb64749470093b3433c',
+    storageBucket: 'holiday-planner-8b6a7.appspot.com',
+    apiKey: 'AIzaSyAN8cLNIGwBMV2v4j0fMylIYMu30t4PuU0',
+    authDomain: 'holiday-planner-8b6a7.firebaseapp.com',
+    messagingSenderId: '739095630197',
+  },};
